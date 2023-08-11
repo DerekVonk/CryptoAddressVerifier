7296c8191ad5244b198114e3758b5bd67dfc6462
Derek Vonk <derekvonk@me.com>
2023-08-11 09:19:24 +0200

    add validation on btc addresses

--- cav.py
+++ cav.py
@@ -5,8 +5,11 @@ import requests
 from eip55 import test
 from web3 import Web3
 from decouple import config
+import base58
+from pycoin.contrib import bech32m
 
 ETHER_SCAN_API_KEY = config('ETHER_SCAN_API_KEY')
+
 
 def is_valid_ethereum_address(address):
     """Returns True if the address is a valid Ethereum address, False otherwise."""
@@ -18,19 +21,14 @@ def is_valid_ethereum_address(address):
 def is_valid_bitcoin_address(address):
     """Returns True if the address is a valid Bitcoin address, False otherwise."""
 
-    if len(address) != 26:
+    if address[0] == "1":  # P2PKH Address
+        return base58.b58decode_check(address)
+    elif address[0] == "3":  # P2SH Address
+        return base58.b58decode_check(address)
+    elif address[:3] == "bc1":  # Bech32 Addresses (Segwit)
+        return bech32m.bech32_decode(address)
+    else:
         return False
-
-    address_without_prefix = address[2:]
-
-    checksum = address[0:2]
-    sha256_hash = hashlib.sha256(address_without_prefix.encode())
-    double_sha256_hash = hashlib.sha256(sha256_hash.digest()).hexdigest()[:4]
-
-    if checksum != double_sha256_hash:
-        return False
-
-    return True
 
 
 def is_valid_cardano_address(address):
@@ -60,7 +58,7 @@ def is_valid_cardano_address(address):
 def is_same_address(address_a, address_b):
     """Returns True if the two addresses are the same, False otherwise."""
 
-    return address_a == address_b
+    return address_a.lower() == address_b.lower()
 
 
 def is_valid_address(address, network):
@@ -102,14 +100,15 @@ def is_non_zero_ethereum_address(address):
 def is_used_bitcoin_address(address):
     """Returns True if the address has been used before, False otherwise."""
 
-    url = "https://blockchair.com/bitcoin/address/{}".format(address)
+    url = "https://chain.api.btc.com/v3/address/{}".format(address)
     response = requests.get(url)
+    response.raise_for_status()
 
-    if response.status_code == 200:
-        data = json.loads(response.content)
-
-        if "balance" in data:
-            return True
+    data = json.loads(response.content)
+    amount = data['data']['balance']
+    if amount:
+        amount /= 100_000_000
+        return amount
 
     return False
 
@@ -174,12 +173,20 @@ def main():
 
         amount = is_used_address(address_b, network)
         if amount:
-            print("Second Address '{}'\n\thas non-zero amount of {} {}".format(address_b, amount, network))
+            print("Address has non-zero amount of {} {}".format(amount, network))
 
     # We're checking if the address is valid
     else:
-        if not is_valid_address(address_a, network):
-            print("address is Invalid '{}'.".format(address_a))
+        if is_valid_address(address_a, network):
+            print("address is valid '{}'.".format(address_a))
+
+            amount = is_used_address(address_a, network)
+            if amount:
+                print("Address has non-zero amount of {} {}".format(amount, network))
+
+            return
+        else:
+            print("address is INVALID '{}'.".format(address_a))
             return
 
 
